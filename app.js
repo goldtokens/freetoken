d63189ed6b975a22d773d52fc776326af2d39696
goldtokens <127283284+goldtokens@users.noreply.github.com>
2023-03-08 16:50:55 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// first image
+let svg = document.getElementById('svg');
+function firstFunction() {
+    svg.style.fill = "#FD1C68";
+}
+
+let svg_2 = document.getElementById('svg_2');
+function secondFunction() {
+    svg_2.style.fill = "#fdae8f";
+}
+
+let svg_3 = document.getElementById('svg_3');
+function thirdFunction() {
+    svg_3.style.fill = "#519bfd";
+}
+
+//second image
+let svg_4 = document.getElementById('svg_4');
+function forthFunction() {
+    svg_4.style.fill = "#FD1C68";
+}
+
+let svg_5 = document.getElementById('svg_5');
+function fifthFunction() {
+    svg_5.style.fill = "#fdae8f";
+}
+
+let svg_6 = document.getElementById('svg_6');
+function sixthFunction() {
+    svg_6.style.fill = "#519bfd";
+}
+
+//third image
+let svg_7 = document.getElementById('svg_7');
+function seventhFunction() {
+    svg_7.style.fill = "#FD1C68";
+}
+
+let svg_8 = document.getElementById('svg_8');
+function engthFunction() {
+    svg_8.style.fill = "#fdae8f";
+}
+
+let svg_9 = document.getElementById('svg_9');
+function ninthFunction() {
+    svg_9.style.fill = "#519bfd";
+}
+
+
+//forth image
+let svg_10 = document.getElementById('svg_10');
+function tenthFunction() {
+    svg_10.style.fill = "#FD1C68";
+}
+
+let svg_11 = document.getElementById('svg_11');
+function eleventhFunction() {
+    svg_11.style.fill = "#fdae8f";
+}
+
+let svg_12 = document.getElementById('svg_12');
+function twelvthFunction() {
+    svg_12.style.fill = "#519bfd";
+}
+
+//third section
+let svg_13 = document.getElementById('svg-13');
+function tertinthFunction() {
+    svg_13.style.fill = "#FD1C68";
+}
+let svg_14 = document.getElementById('svg_14');
+function forteenthFunction() {
+    svg_14.style.fill = "#FD1C68";
+}
+let svg_15 = document.getElementById('svg-15');
+function fifteenthFunction() {
+    svg_15.style.fill = "#FD1C68";
+}
